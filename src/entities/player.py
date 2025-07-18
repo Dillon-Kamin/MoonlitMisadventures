94d83fadd11fe63209656e67794b7efa5461ad39
dillong7kamin@gmail.com <dillong7kamin@gmail.com>
2025-07-17 20:01:31 -0700

    Shifted to sprite based player

--- src/entities/player.py
+++ src/entities/player.py
@@ -1,23 +1,29 @@
 # src/entities/player.py
 import pygame as pg
 
-class Player:
-    def __init__(self, x, y, speed=200):
-        self.rect = pg.Rect(x, y, 32, 32)
-        self.color = pg.Color("dodgerblue")
-        self.speed = speed
+class Player(pg.sprite.Sprite):
+    def __init__(self, pos, group):
+        super().__init__(group)
 
-    def handle_event(self, event):
-        pass
+        # visual
+        self.image = pg.Surface((32, 64))
+        self.image.fill("green")
+        self.rect = self.image.get_rect(center = pos)
 
-    def update(self, dt, keys):
-        dx = dy = 0
-        if keys[pg.K_w]: dy -= self.speed * dt
-        if keys[pg.K_s]: dy += self.speed * dt
-        if keys[pg.K_a]: dx -= self.speed * dt
-        if keys[pg.K_d]: dx += self.speed * dt
-        self.rect.x += dx
-        self.rect.y += dy
+        # movement
+        
 
-    def render(self, surface):
-        pg.draw.rect(surface, self.color, self.rect)
+    def input(self):
+        keys = pg.key.get_pressed()
+
+        if keys[pg.K_UP]:
+            pass
+        elif keys[pg.K_DOWN]:
+            pass
+        if keys[pg.K_RIGHT]:
+            pass
+        elif keys[pg.K_LEFT]:
+            pass
+
+    def update(self, dt):
+        self.input()
--- src/states/GameplayDevState.py
+++ src/states/GameplayDevState.py
@@ -10,7 +10,12 @@ class GamePlayState(BaseState):
     def __init__(self, context):
         self.context = context
         self.quit = False
-        self.player = Player(100, 100)
+
+        self.all_sprites = pg.sprite.Group()
+        self.setup()
+
+    def setup(self):
+        self.player = Player(pos=(640, 360), group=self.all_sprites)
     
     def handle_event(self, event):
         if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
@@ -20,8 +25,8 @@ class GamePlayState(BaseState):
 
     def update(self, dt):
         keys = pg.key.get_pressed()
-        self.player.update(dt, keys)
+        self.all_sprites.update(dt)
 
     def render(self, screen):
-        screen.fill(pg.Color("darkgreen"))
-        self.player.render(screen)
+        screen.fill(pg.Color("black"))
+        self.all_sprites.draw(screen)

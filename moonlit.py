9d0e5dde7fa3c3fba02e22890099283febaabad7
Dillon <43020432+Dillon-Kamin@users.noreply.github.com>
2025-08-13 14:11:57 -0700

    Update moonlit.py

--- moonlit.py
+++ moonlit.py
@@ -1,12 +1,4 @@
-"""
-This game is an effort for me to expose myself to Python on a regular basis to gain
-a good amount of muscle memory when it comes to syntax and fundamental methods and
-data types in the language. 
-
-Royalty tax to Michael Purcell: 1%
-
-~Dillon, July 4, 2025
-"""
+# moonlit.py
 
 import sys
 import pygame as pg
